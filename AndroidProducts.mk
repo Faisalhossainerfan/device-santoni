3614fdac957df7c498ac3c5ef4b2324938b2efa2
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-12 23:39:32 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# CipherOS
+# YaapOS
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/yaap_santoni.mk
 
 COMMON_LUNCH_CHOICES += \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    yaap_santoni-userdebug \
+    yaap_santoni-user \
+    yaap_santoni-eng
