2787feeec8740052538c54dbda49dd5cc797ae86
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-31 19:41:10 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# HavocOS
+# PixelExperience
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/havoc_santoni.mk
+    $(LOCAL_DIR)/aosp_santoni.mk
 
 COMMON_LUNCH_CHOICES += \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
+    aosp_santoni-userdebug \
+    aosp_santoni-user \
+    aosp_santoni-eng
